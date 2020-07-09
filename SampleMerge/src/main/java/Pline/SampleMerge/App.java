b3c0c26505e6c974ddc5c1657e724c0b6d74fb28
lakshmi.gr <lakshmi.gr@192.168.1.102>
2020-07-09 13:27:49 +0530

    Adding sampleMerge to dev1

---
+++ SampleMerge/src/main/java/Pline/SampleMerge/App.java
@@ -0,0 +1,23 @@
+package Pline.SampleMerge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void SampleMethod() {
+		
+    	int a = 0;
+    	int b = 1;
+    	a = a+b;
+    	
+    	System.out.println( "value of a"+a );
+    	
+	}
+}
